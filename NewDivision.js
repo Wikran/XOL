7fe24491e23604c52b6cd835010c4c97270bc4cb
wikran <wikran@hotmail.com>
2024-09-28 09:51:55 +0700

    Main Check the change control

---
+++ NewDivision.js
@@ -0,0 +1,9 @@
+const newAaDivision =  [
+    { AccDivCode: "1128-07" },
+    { AccDivCode: "1186-14" },
+    { AccDivCode: "1186-15" },
+    { AccDivCode: "1186-16" }, 
+    { AccDivCode: "1186-17" },
+    { AccDivCode: "1186-18" }, 
+    { AccDivCode: "1186-19" },          
+]
